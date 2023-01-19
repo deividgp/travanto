4a7e3473e0e33e73d20fc8fd4b8e55afdb157a5f
Deividgp <dayd4@outlook.com>
2023-01-19 16:55:19 +0100

    Update Seeders

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,30 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('users')->insert([
+            'name' => 'David Gutiérrez Pasqual',
+            'email' => 'dayd4@outlook.com',
+            'password' => 'deividgp95',
+        ]);
+        DB::table('rentals')->insert([
+            'name' => 'Apartment',
+            'location' => 'Barcelona',
+            'rooms' => 2,
+        ]);
+        DB::table('rentals')->insert([
+            'name' => 'Room',
+            'location' => 'Reus',
+            'rooms' => 1,
+        ]);
+        DB::table('rentals')->insert([
+            'name' => 'Apartment',
+            'location' => 'Barcelona',
+            'rooms' => 3,
+        ]);
+        DB::table('rentals')->insert([
+            'name' => 'House',
+            'location' => 'Hamburg',
+            'rooms' => 4,
+        ]);
     }
 }
